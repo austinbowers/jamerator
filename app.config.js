df5c1575334573e0f1294233a9ecf746ad1ce465
austinbowers <austin.bowers75@gmail.com>
2025-03-24 11:05:54 -0500

    Fix config.

--- app.config.js
+++ app.config.js
@@ -5,48 +5,55 @@ export default {
         scheme: 'com.austinbowers.jamerator',
         extra: {
             OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-            "eas": {
-                "projectId": "41ff8154-9395-4c0c-b363-66f8193d4b32"
+            eas: {
+                projectId: "41ff8154-9395-4c0c-b363-66f8193d4b32"
             },
         },
         icon: "./assets/images/Jamerator_Icon.png",
-        "plugins": [
+        splash: {
+            image: "./assets/icons/splash-icon-light.png",  // Ensure image is specified here
+            resizeMode: "contain",
+            backgroundColor: "#0A130E",
+            dark: {
+                image: "./assets/icons/splash-icon-dark.png",
+                backgroundColor: "#0A130E"
+            }
+        },
+        plugins: [
+            "expo-sqlite",
+            "expo-router",
             [
-                "expo-sqlite",
-                "expo-router",
-                [
-                    "expo-splash-screen",
-                    {
-                        "backgroundColor": "#ffffff",
-                        "image": "./assets/icon/splash-icon-light.png",
-                        "dark": {
-                            "image": "./assets/icon/splash-icon-dark.png",
-                            "backgroundColor": "#000000"
-                        },
-                        "imageWidth": 200
-                    }
-                ],
+                "expo-splash-screen",
+                {
+                    backgroundColor: "#ffffff",
+                    image: "./assets/icons/splash-icon-light.png",
+                    dark: {
+                        image: "./assets/icons/splash-icon-dark.png",
+                        backgroundColor: "#0A130E"
+                    },
+                    imageWidth: 200
+                }
             ]
         ],
-        "android": {
+        android: {
             "package": "com.austinbowers.jamerator"
         },
-        "ios": {
+        ios: {
             "bundleIdentifier": "com.austinbowers.jamerator",
             "infoPlist": {
                 "ITSAppUsesNonExemptEncryption": false
             },
-            "icon": {
+            icon: {
                 "dark": "./assets/icons/ios-dark.png",
                 "light": "./assets/icons/ios-light.png",
                 "tinted": "./assets/icons/ios-tinted.png",
             },
         },
-        "newArchEnabled": true,
-        "updates": {
+        newArchEnabled: true,
+        updates: {
             "url": "https://u.expo.dev/41ff8154-9395-4c0c-b363-66f8193d4b32"
         },
-        "runtimeVersion": {
+        runtimeVersion: {
             "policy": "appVersion"
         },
     },
