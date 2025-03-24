49d4169c898809b61038bad1b0efb926be451b03
austinbowers <austin.bowers75@gmail.com>
2025-03-24 10:11:46 -0500

    Auto Build on Push. Add icons.

--- app.config.js
+++ app.config.js
@@ -5,19 +5,53 @@ export default {
         scheme: 'com.austinbowers.jamerator',
         extra: {
             OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+            "eas": {
+                "projectId": "41ff8154-9395-4c0c-b363-66f8193d4b32"
+            },
         },
+        "icon": "./assets/images/Jamerator_Icon.png",
         "plugins": [
             [
                 "expo-sqlite",
                 "expo-router",
+                "expo-splash-screen",
+                {
+                    image: "./assets/icons/splash-icon-dark.png",
+                    imageWidth: 200,
+                    resizeMode: "contain",
+                    backgroundColor: "#ffffff",
+                    dark: {
+                        image: "./assets/icons/splash-icon-light.png",
+                        backgroundColor: "#000000",
+                    }
+                }
             ]
         ],
         "android": {
-            "package": "com.austinbowers.jamerator"
+            "package": "com.austinbowers.jamerator",
+            "adaptiveIcon": {
+                foregroundImage: "./assets/icons/adaptive-icon.png",
+                monochromeImage: "./assets/icons/adaptive-icon.png",
+                backgroundColor: "#ffffff",
+            }
         },
         "ios": {
-            "bundleIdentifier": "com.austinbowers.jamerator"
+            "bundleIdentifier": "com.austinbowers.jamerator",
+            "infoPlist": {
+                "ITSAppUsesNonExemptEncryption": false
+            },
+            "icon": {
+                dark: "./assets/icons/ios-dark.png",
+                light: "./assets/icons/ios-light.png",
+                tinted: "./assets/icons/ios-tinted.png",
+            }
         },
         "newArchEnabled": true,
+        "updates": {
+            "url": "https://u.expo.dev/41ff8154-9395-4c0c-b363-66f8193d4b32"
+        },
+        "runtimeVersion": {
+            "policy": "appVersion"
+        },
     },
 };
