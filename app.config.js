70f453610bd67616027f50cc3574a32035af2f08
austinbowers <austin.bowers75@gmail.com>
2025-03-24 10:41:36 -0500

    Remove workflow.

--- app.config.js
+++ app.config.js
@@ -9,42 +9,25 @@ export default {
                 "projectId": "41ff8154-9395-4c0c-b363-66f8193d4b32"
             },
         },
-        "icon": "./assets/images/Jamerator_Icon.png",
         "plugins": [
             [
                 "expo-sqlite",
                 "expo-router",
-                "expo-splash-screen",
-                {
-                    image: "./assets/icons/splash-icon-dark.png",
-                    imageWidth: 200,
-                    resizeMode: "contain",
-                    backgroundColor: "#ffffff",
-                    dark: {
-                        image: "./assets/icons/splash-icon-light.png",
-                        backgroundColor: "#000000",
-                    }
-                }
             ]
         ],
         "android": {
-            "package": "com.austinbowers.jamerator",
-            "adaptiveIcon": {
-                foregroundImage: "./assets/icons/adaptive-icon.png",
-                monochromeImage: "./assets/icons/adaptive-icon.png",
-                backgroundColor: "#ffffff",
-            }
+            "package": "com.austinbowers.jamerator"
         },
         "ios": {
             "bundleIdentifier": "com.austinbowers.jamerator",
             "infoPlist": {
                 "ITSAppUsesNonExemptEncryption": false
             },
-            "icon": {
+            icon: {
                 dark: "./assets/icons/ios-dark.png",
                 light: "./assets/icons/ios-light.png",
                 tinted: "./assets/icons/ios-tinted.png",
-            }
+            },
         },
         "newArchEnabled": true,
         "updates": {
